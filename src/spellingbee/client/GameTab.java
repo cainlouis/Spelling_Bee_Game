47a0d943a8a44c850374f39574128afdec5a6108
Nael Louis <nael.louis1002@gmail.com>
2020-12-07 10:13:44 -0500

    Made changes to GameTab

---
+++ src/spellingbee/client/GameTab.java
@@ -0,0 +1,11 @@
+package spellingbee.client;
+
+import java.awt.TextField;
+
+import javafx.scene.*;
+
+public class GameTab extends Tab {
+	public GameTab() {
+		TextField tx = new TextField("Hello");
+	}
+}
