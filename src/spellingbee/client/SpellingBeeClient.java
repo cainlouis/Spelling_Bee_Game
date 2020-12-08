d7d06bdd57b50cc470c82923d7ad92588a557365
Nael Louis <nael.louis1002@gmail.com>
2020-12-08 18:34:42 -0500

    Modifications made to SpellingBeeClient

--- src/spellingbee/client/SpellingBeeClient.java
+++ src/spellingbee/client/SpellingBeeClient.java
@@ -5,10 +5,15 @@ import javafx.application.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.control.TabPane.TabClosingPolicy;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
 import javafx.stage.*;
 
+/**
+ * This class create the stage for the GameTab, the ScoreTab, and the HighScore tab. Create a client object which is passed to the tabs. 
+ * @author Nael Louis
+ *
+ */
 public class SpellingBeeClient extends Application {
 	private Client client = new Client();
 	
@@ -16,9 +21,13 @@ public class SpellingBeeClient extends Application {
 		Group root = new Group();
 		TabPane tab = new TabPane();
 		GameTab game = new GameTab(client);
+		//ScoreTab score = new ScoreTab(client);
+		//HighScore highScore = new HighScore(Client);
 		VBox overall = new VBox();
 		
 		tab.getTabs().add(game);
+		//tab.getTabs().add(score);
+		//tab.getTabs.add(highScore);
 		tab.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
 		overall.getChildren().add(tab);
 		root.getChildren().add(overall);
